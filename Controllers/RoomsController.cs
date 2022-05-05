74a28dbc8d3ddea93c9cf837577009f694ccde62
Cooper <mickeylyc@outlook.com>
2022-05-05 13:37:33 +0800

    增加了Room主页面表格的筛选逻辑：新增了一个ViewModel；更改了RoomsController的内容；更改了Room的Index页面显示

--- Controllers/RoomsController.cs
+++ Controllers/RoomsController.cs
@@ -7,10 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using HotelBookingSystem.Data;
 using HotelBookingSystem.Models;
+using HotelBookingSystem.ViewModels;
 
 namespace HotelBookingSystem.Controllers
 {
-    public class RoomsController : Controller
+    public class RoomsController : Controller   // TODO:正修改此控制器来添加搜索服务
     {
         private readonly HotelBookingSystemContext _context;
 
@@ -19,11 +20,63 @@ namespace HotelBookingSystem.Controllers
             _context = context;
         }
 
-        // GET: Rooms
-        public async Task<IActionResult> Index()
+        // // GET: Rooms or Rooms?searchString=
+        // public async Task<IActionResult> Index(string searchString) // 增加了根据名称查找的功能
+        // {
+        //     var hotelBookingSystemContext = _context.Room.Include(r => r.Hotel).Include(r => r.RoomType);
+        //     var rooms = from room in hotelBookingSystemContext select room;
+        //     if (!String.IsNullOrEmpty(searchString))
+        //     {
+        //         rooms = rooms.Where(s => s.Name.Contains(searchString));
+        //     }
+        //     return View(await rooms.ToListAsync());
+        // }
+
+        // GET: Rooms (Advanced methods)    // TODO: 注意这里筛选功能的写法！
+        public async Task<IActionResult> Index(string roomType, string roomHotel, string roomName)
         {
-            var hotelBookingSystemContext = _context.Room.Include(r => r.Hotel).Include(r => r.RoomType);
-            return View(await hotelBookingSystemContext.ToListAsync());
+            var hotelBookingSystemContext = _context.Room
+                .Include(r => r.Hotel)
+                .Include(r => r.RoomType);
+            
+            // using LINQ
+            IQueryable<string> hotelQuery = // TODO:设置查询语句
+                from r in hotelBookingSystemContext orderby r.Hotel.Name select r.Hotel.Name;
+
+            IQueryable<string> typeQuery = // TODO:设置查询语句
+                from r in hotelBookingSystemContext orderby r.RoomType.Id select r.RoomType.Name;
+
+            var rooms = from room in hotelBookingSystemContext select room; // TODO:获取全部房间
+
+            if (!String.IsNullOrEmpty(roomName))
+            {
+                rooms = rooms.Where(s => s.Name.Contains(roomName));    // TODO:简单筛选房间名称
+            }
+
+            if (!String.IsNullOrEmpty(roomType))
+            {
+                rooms = rooms.Where(x => x.RoomType.Name == roomType);  // TODO:筛选房间类型
+            }
+
+            if (!String.IsNullOrEmpty(roomHotel))
+            {
+                rooms = rooms.Where(x => x.Hotel.Name == roomHotel);    // TODO:筛选所属酒店
+            }
+
+            var roomSearchViewModel = new RoomSearchViewModel
+            {
+                Hotels = new SelectList(await hotelQuery.Distinct().ToListAsync()), // TODO:设置下拉菜单
+                RoomTypes = new SelectList(await typeQuery.Distinct().ToListAsync()),   // TODO:设置下拉菜单
+                Rooms = await rooms.ToListAsync()
+            };
+
+            return View(roomSearchViewModel);
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
         }
 
         // GET: Rooms/Details/5
---
+++ ViewModels/RoomSearchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using HotelBookingSystem.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HotelBookingSystem.ViewModels
+{
+    public class RoomSearchViewModel
+    {
+        public List<Room> Rooms { get; set; }   // 存放要显示的Rooms
+
+        public SelectList RoomTypes { get; set; }   // 这里并不是直接从Model中拿数据，而是DB搜索结果填充在这里
+
+        public SelectList Hotels { get; set; }   // 这里并不是直接从Model中拿数据，而是DB搜索结果填充在这里
+
+        public string RoomType { get; set; }    // 搜索词
+
+        public string RoomHotel { get; set; }   // 搜索词
+
+        public string RoomName { get; set; }    // 搜索词
+    }
+}
